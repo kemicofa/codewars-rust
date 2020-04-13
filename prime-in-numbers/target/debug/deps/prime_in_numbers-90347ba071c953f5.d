abf070af2c2eb3f573ca0aec73fbce6db53401ff
kemicofa <einar.ase.91@gmail.com>
2020-04-14 00:28:01 +0200

    fixed gitignore location

--- prime-in-numbers/target/debug/deps/prime_in_numbers-90347ba071c953f5.d
+++
@@ -1,5 +0,0 @@
-/Users/anonymous/Documents/rust/codewars-rust/prime-in-numbers/target/debug/deps/prime_in_numbers-90347ba071c953f5: src/main.rs
-
-/Users/anonymous/Documents/rust/codewars-rust/prime-in-numbers/target/debug/deps/prime_in_numbers-90347ba071c953f5.d: src/main.rs
-
-src/main.rs:
