061c5a165cf25cc82330329e0fefea5b3d5a2ea3
kemicofa <einar.ase.91@gmail.com>
2020-04-14 01:05:13 +0200

    wip

--- prime-in-numbers/target/debug/prime_in_numbers-90347ba071c953f5.d
+++
@@ -1,1 +0,0 @@
-/Users/anonymous/Documents/rust/codewars-rust/prime-in-numbers/target/debug/prime_in_numbers-90347ba071c953f5: /Users/anonymous/Documents/rust/codewars-rust/prime-in-numbers/src/main.rs
